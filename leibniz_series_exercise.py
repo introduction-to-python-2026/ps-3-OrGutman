efacf0c38abebdc5d69235d16e66e1d047d0b832
OrGutman <or.gutman7@gmail.com>
2025-11-06 14:02:10 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,5 +1,5 @@
 def approximate_pi(n_terms):
     pi = 0
     for i in range(n_terms):
-        pi += 4 * (((-1)**i) / (2*i + 1))
-    return pi
+        pi += 4 * ((-1)**i / (2*i + 1))
+    return round(pi, 4)
